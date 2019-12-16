57d3fa47a6e356c4892a2aac72ebb3530e1635f2
Al <seeds.bite@gmail.com>
2019-12-16 23:09:28 +0300

    fix save files to directory

--- src/main/java/by/rgs/demo/service/impl/FileServiceImpl.java
+++ src/main/java/by/rgs/demo/service/impl/FileServiceImpl.java
@@ -3,6 +3,8 @@ package by.rgs.demo.service.impl;
 import java.io.File;
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,13 +15,25 @@ import by.rgs.demo.service.FileService;
 @Service
 public class FileServiceImpl implements FileService {
 
-	public static final String uploadingDirectory = System.getProperty("user.dir") + "\\src\\main\\resources\\reports\\";
+	private static final Logger log = LoggerFactory.getLogger(FileServiceImpl.class);
+	public static final String uploadingDirectory = System.getProperty("user.dir")
+			+ "\\src\\main\\resources\\reports\\";
 
 	@Override
 	public Message uploadFiles(MultipartFile[] files) {
 		if (files.length != 0) {
+			File directory = new File(uploadingDirectory);
+			boolean isExistDirectory = checkExistDirectory(directory);
+			if (!isExistDirectory) {
+				try {
+					directory.mkdir();
+				} catch (Exception e) {
+					return new Message(HttpStatus.INTERNAL_SERVER_ERROR, "Не удалось создать директорию на сервере");
+				}
+			}
+			File file;
 			for (MultipartFile multipartFile : files) {
-				File file = new File(uploadingDirectory + multipartFile.getOriginalFilename());
+				file = new File(uploadingDirectory + multipartFile.getOriginalFilename());
 				try {
 					multipartFile.transferTo(file);
 				} catch (IllegalStateException e) {
@@ -36,4 +50,7 @@ public class FileServiceImpl implements FileService {
 		}
 	}
 
+	private boolean checkExistDirectory(File directory) {
+		return directory.exists();
+	}
 }
