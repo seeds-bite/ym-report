7d82121f0380fb41f3e2ec8117f4a808442821d6
Al <seeds.bite@gmail.com>
2020-01-05 15:57:24 +0300

    add logging

--- src/main/java/by/rgs/demo/controller/MetricsConfigurationController.java
+++ src/main/java/by/rgs/demo/controller/MetricsConfigurationController.java
@@ -2,6 +2,8 @@ package by.rgs.demo.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -23,6 +25,7 @@ import by.rgs.demo.service.UpdateReportService;
 
 @RestController
 public class MetricsConfigurationController {
+	private static final Logger log = LoggerFactory.getLogger(MetricsConfigurationController.class);
 
 	@Autowired
 	UpdateReportService updateReportService;
@@ -33,6 +36,7 @@ public class MetricsConfigurationController {
 
 	@PostMapping(path = "/update-xslx", /* consumes = MediaType.APPLICATION_JSON_VALUE, */ produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> updateXSLXReport(@RequestBody MetricsConfiguration metricsConf) {
+		log.debug("Пришло metricsConf.getIdCount(): " + metricsConf.getIdCount());
 		Message message = updateReportService.updateReport(metricsConf);
 		System.out.println("metricsConf " + metricsConf.getIdCount());
 		return new ResponseEntity<Object>(message, null, message.getStatus());
@@ -40,18 +44,21 @@ public class MetricsConfigurationController {
 	
 	@PostMapping(path = "/uploadFiles")
 	public ResponseEntity<?> uploadFiles(@RequestParam("files") MultipartFile[] files) {
+		log.debug("Пришло files[0].getName(): " + files[0].getName());
 		Message message = fileService.uploadFiles(files);
 		return new ResponseEntity<Object>(message, null, message.getStatus());
 	}
 	
 	@PostMapping(path = "/code")
 	public ResponseEntity<?> uploadFiles(@RequestParam("code") String code) {
+		log.debug("Пришло code: " + code);
 		UserProfile profile = requestService.executeRequestForToken(code);
 		return new ResponseEntity<Object>(profile, null, HttpStatus.OK);
 	}
 	
 	@GetMapping(path = "/getCounters")
 	public ResponseEntity<?> getCounters() {
+		log.debug("getCounters запрос");
 		System.out.println("get Counters controller");
 		List<Counter> counters = requestService.getUserCounters();
 		return new ResponseEntity<Object>(counters, null, HttpStatus.OK);
@@ -59,6 +66,7 @@ public class MetricsConfigurationController {
 	
 	@GetMapping(path = "/download")
 	public ResponseEntity<?> downloadReports() {
+		log.debug("downloadReports запрос");
 		ResponseEntity<?> files = fileService.downloadFiles();
 		return files;
 
